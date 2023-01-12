7f57e43d2f0ccf3acd6d84507142bfd749343ead
Vladimir Kuznetsov <VladKuznetsov@croc.ru>
2023-01-12 18:08:52 +0300

    change loader

--- app/web/load.py
+++ app/web/load.py
@@ -9,10 +9,10 @@ from app.constants import default_timeout, default_timeout_value
 bp = Blueprint("load", __name__)
 
 
-def do_fibonacci() -> (int, int):
-    f1, f2 = 0, 1  # 1, 2, 3, 5, 8, 13, 21, 44...
+def do_math():
     while True:
-        f1, f2 = f2, f1 + f2
+        # noinspection PyStatementEffect
+        65535 * 65535
 
 
 @bp.route("/load")
@@ -26,7 +26,7 @@ def load():
     )
     processes = list()
     for _ in range(multiprocessing.cpu_count()):
-        process = multiprocessing.Process(target=do_fibonacci)
+        process = multiprocessing.Process(target=do_math)
         process.start()
         processes.append(process)
     time.sleep(timeout)
