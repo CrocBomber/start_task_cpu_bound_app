3c98b3bf58790b000ed4da06fd434a07a16917a6
Vladimir Kuznetsov <VladKuznetsov@croc.ru>
2023-01-12 14:12:59 +0300

    improve info

--- app/web/info.py
+++ app/web/info.py
@@ -1,12 +1,42 @@
+import http
+import logging
+import urllib.error
 import urllib.request
 
 from flask import Blueprint
 
+logger = logging.getLogger(__name__)
 bp = Blueprint("info", __name__)
+
+
+def retrieve_data(url: str) -> str | None:
+    try:
+        with urllib.request.urlopen(url) as f:
+            logger.info(url, f.status)
+            return f.read().decode()
+    except urllib.error.HTTPError as ex:
+        logger.warning(url, ex.status)
+        if ex.status == http.HTTPStatus.NOT_FOUND:
+            return None
+        else:
+            raise ex
+
+
+def walk(url: str) -> str | dict | None:
+    value = retrieve_data(url)
+    if not value:
+        return None
+    if url.endswith("/"):
+        info = dict()
+        for node in value.split("\n"):
+            info[node.rstrip("/")] = walk(url + node)
+        return info
+    else:
+        return value
 
 
 @bp.route("/info")
 def info():
     url = "http://169.254.169.254/latest/meta-data/"
-    with urllib.request.urlopen(url) as f:
-        return f.read()
+    result = walk(url)
+    return result
